250e421e05af493f82c341d7cddbe264f7b02a20
James <jafsheridan@gmail.com>
2011-11-06 13:01:39 -0800

    started algorithms

--- snowflake/clothes/static/js/fit.js
+++ snowflake/clothes/static/js/fit.js
@@ -1,24 +1,90 @@
 function calcDiff(ref, comp)
 {
 	var nutCupping = nutCup(ref, comp);
-	
-	return nutCupping;
+	var legTightness = legFit(ref, comp);
+	var buttCupping = buttCup(ref, comp);
+	var diaperButt = diaper(comp, buttCupping);
+	var muffinTop = muffin(ref, comp);
+	var thighTightness = thigh(ref, comp); 
+	var ankleTightness = ankle(ref, comp);
+	return "nuts:"+nutCupping+
+		  " leg:"+legTightness+
+		  " buttCup:"+buttCupping+ 
+		  " diaper:"+diaperButt+
+		  " muffin:"+muffinTop+
+		  " thigh:"+thighTightness+
+		  " ankle:"+ankleTightness;
+}
+
+function thigh(ref, comp)
+{
+	return ref["thigh"] - comp["thigh"];
+}
+
+function ankle(ref, comp)
+{
+	return ref["cuff"] - comp["cuff"];
+}
+
+function muffin(ref, comp)
+{
+	return ref["front_rise"] - comp["front_rise"];
+}
+
+function diaper(pant, buttCupping)
+{
+	var excessThreshold = 1;
+	var seamThreshold = .25;
+	if (buttCupping > excessThreshold)
+	{
+		if (seamRatio(pant) > seamThreshold)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+function seamRatio(pant)
+{
+	return pant["outseam"]/pant["inseam"];
+}
+
+function buttCup(ref, comp)
+{
+	var revCup = ref["back_rise"]/riseSum(ref);
+	var compCup = comp["back_rise"]/riseSum(comp);
+	var cuppingFactor = revCup-compCup;
+	return cuppingFactor;
 }
 
 function nutCup(ref, comp)
 {
-	var riseFactor = riseSum(ref, comp);
-	var waistFactor = waist(ref, comp);
+	var riseFactor = riseSum(ref)-riseSum(comp);
+	var waistFactor = ref["waist"]-comp["waist"];
 	var totalFactor = riseFactor + waistFactor;
 	return totalFactor;
 }
 
-function riseSum(ref, comp)
+function riseSum(pant)
 {
-	return (ref["front_rise"] + ref["back_rise"])-(comp["front_rise"] + comp["back_rise"]);
+	return pant["front_rise"] + pant["back_rise"]; 
 }
 
-function waist(ref, comp)
+function legFit(ref, comp)
 {
-	return ref["waist"]-comp["waist"];
+	var refOpening = leg(ref)/body(ref);
+	var compOpening = leg(comp)/body(comp);
+	var openingFactor = refOpening-compOpening;
+	return openingFactor
+}
+
+function leg(pant)
+{
+	return pant["cuff"]+pant["knee"]+pant["thigh"];
+}
+
+function body(pant)
+{
+	return pant["waist"] + riseSum(pant) + pant["outseam"];
 }
--- snowflake/fitdict.py
+++ snowflake/fitdict.py
@@ -1,4 +1,5 @@
 FIT_DICT = { #all are ref pant MINUS returned pant
+#These are mostly done, ready for James
 'nutcupping': # a shorter totalRise without a larger waist will result in more nutcupping
 {
 	'zero':'These pants will fit you in the crotch with about the same level of tightness',
@@ -15,85 +16,43 @@ FIT_DICT = { #all are ref pant MINUS returned pant
 		3: 'These pants will feel a lot less tight in the crotch.'
 		}
 	},
-
-'backgap': # a larger (Outseam - Inseam), assuming equal totalRise, will equal less backgap
-# a larger (O-I) with shorter totalRise will equal more wedgie and nutcupping (should add a variable for wedgie)
-# a larger (O-I) with longer totalRise will equal larger pants
+	
+'leg': #a function of (Cuff + Knee + Thigh) divided by (waist + totalRise + outseam)
 {
-	'zero':"These pants won't change how much they gap at the small of your back",
+	'zero':'The leg will feel generally the same degree of tightness',
 	'positive':
 		{
-		1: 'nada',
-		2: 'nada',
-		3: 'nada'
+		1: 'The leg will feel slightly more tight all the way down',
+		2: 'The leg will feel quite a bit more tight all the way down',
+		3: 'The leg will feel a lot more tight all the way down'
 		},
 	'negative':
 		{
-		1: 'nada',
-		2: 'nada',
-		3: 'nada'
-		}
-	},	
-
-'wedgie': # a larger (O-I) with shorter totalRise will equal more wedgie and nutcupping
-
-{
-	'zero':"These pants won't change how much they gap at the small of your back",
-	'positive':
-		{
-		1: 'nada',
-		2: 'nada',
-		3: 'nada'
-		},
-	'negative':
-		{
-		1: 'nada',
-		2: 'nada',
-		3: 'nada'
+		1: 'The leg will feel slightly more loose all the way down',
+		2: 'The leg will feel quite a bit more loose all the way down',
+		3: 'The leg will feel a lot more loose all the way down'
 		}
 	},		
 
+'buttCupping': # +ratio(backrise/totalrise) if  +ratio(outseam/inseam) < threshold (if >threshold then it's higher back)
+#if -ratio(backrise/totalrise) then more buttcup (OR, we're not sure: either just changed crotch (no impact on fit) or wedgie and high front rise, depending on something we don't understand)
 
-'plumberButt': # increases if backRise goes down and frontRise doesn't increase to compensate
-# if frontRise does increase, the crotch just moves further back (we don't know the impact of that)
 {
-	'zero':"These pants won't change your risk of plumber butt.",
+	'zero':"These pants won't change how much they gap at the small of your back",
 	'positive':
 		{
-		1: 'These pants will increase your risk of plumber butt by a lot.',
-		2: 'These pants will increase your risk of plumber butt by a little bit.'
-
+		1: 'nada',
+		2: 'nada',
+		3: 'nada'
 		},
 	'negative':
 		{
-		1: 'These pants will decrease the chance you get a plumber butt, but not by much.',
-		2: 'These pants should signficanly decrease your risk of getting a plumber butt.'
+		1: 'nada',
+		2: 'nada',
+		3: 'nada'
 		}
 	},	
-	
-'waistLooseness': #increases if waist goes up and frontRise stays the same; decreases if waist decrease and frontRise remains same; ambiguous if waist increases but frontRise decreases; loosens if waist increases and front rise increases
-# need to rewrite these descriptors to account for waist/rise interaction
-{
-	'zero': 'Similar waist', 
-	'positive': 
-		{
-		.25: 'Similar waist',
-		.5: 'Slightly tighter waist',
-		1: 'Tighter waist',
-		2: 'Much tighter waist',
-		99: 'Insanely tighter waist'
-		},
-	'negative': 
-		{
-		.25: 'Similar waist',
-		.5: 'A bit looser waist',
-		1: 'Looser waist',
-		2: 'Much looser waist',
-		99: 'Insanely looser waist'
-		}
-	},
-	
-		
+
 'muffintop': #for now just a function of frontRise
 {
 	'zero':"The pants will sit on your waist at about the same level.",
@@ -111,7 +70,6 @@ FIT_DICT = { #all are ref pant MINUS returned pant
 		}
 	},	
 	
-
 'thigh': #a function of the thigh and the fabric 
 {
 	'zero':'nada',
@@ -151,12 +109,34 @@ FIT_DICT = { #all are ref pant MINUS returned pant
 	},
 
 
-'leg': #a function of (Cuff + Knee + Thigh) divided by (waist + totalRise + outseam)
+'plumberButt': # increases if backRise goes down and frontRise doesn't increase to compensate
+# if frontRise does increase, the crotch just moves further back (we don't know the impact of that)
 {
-	'zero':'The leg will feel generally the same degree of tightness',
+	'zero':"These pants won't change your risk of plumber butt.",
 	'positive':
 		{
-		1: 'The leg will feel slightly more tight all the way down',
+		1: 'These pants will increase your risk of plumber butt by a lot.',
+		2: 'These pants will increase your risk of plumber butt by a little bit.'
+
+		},
+	'negative':
+		{
+		1: 'These pants will decrease the chance you get a plumber butt, but not by much.',
+		2: 'These pants should signficanly decrease your risk of getting a plumber butt.'
+		}
+	},
+
+
+#Not going to use these yet
+'backgap': # a larger (Outseam - Inseam), assuming equal totalRise, will equal less backgap
+# a larger (O-I) with shorter totalRise will equal more wedgie and nutcupping (should add a variable for wedgie)
+# a larger (O-I) with longer totalRise will equal larger pants
+#should have waist too
+{
+	'zero':"These pants won't change how much they gap at the small of your back",
+	'positive':
+		{
+		1: 'nada',
 		2: 'nada',
 		3: 'nada'
 		},
@@ -167,7 +147,50 @@ FIT_DICT = { #all are ref pant MINUS returned pant
 		3: 'nada'
 		}
 	},	
+
+'wedgie': # +rearRise/totalRise
+{
+	'zero':"These pants won't change how much they gap at the small of your back",
+	'positive':
+		{
+		1: 'nada',
+		2: 'nada',
+		3: 'nada'
+		},
+	'negative':
+		{
+		1: 'nada',
+		2: 'nada',
+		3: 'nada'
+		}
+	},		
+
 	
+	
+'waistLooseness': #increases if waist goes up and frontRise stays the same; decreases if waist decrease and frontRise remains same; ambiguous if waist increases but frontRise decreases; loosens if waist increases and front rise increases
+# need to rewrite these descriptors to account for waist/rise interaction
+{
+	'zero': 'Similar waist', 
+	'positive': 
+		{
+		.25: 'Similar waist',
+		.5: 'Slightly tighter waist',
+		1: 'Tighter waist',
+		2: 'Much tighter waist',
+		99: 'Insanely tighter waist'
+		},
+	'negative': 
+		{
+		.25: 'Similar waist',
+		.5: 'A bit looser waist',
+		1: 'Looser waist',
+		2: 'Much looser waist',
+		99: 'Insanely looser waist'
+		}
+	},
+	
+
+
 'seat': #as +(outseam divided by inseam) or +rearrise, seat increases
 {
 	'zero':'The seat has generally more room',
