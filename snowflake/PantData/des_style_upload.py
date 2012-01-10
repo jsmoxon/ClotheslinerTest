12693f084121728ba95ed979c7f72e5465b622d7
Jack Moxon <jsmoxon@gmail.com>
2012-01-09 17:47:18 -0800

    switching for db purposes

--- snowflake/PantData/des_style_upload.py
+++ snowflake/PantData/des_style_upload.py
@@ -14,11 +14,11 @@ dataReader = csv.reader(open(csv_home, "rU"), delimiter=',')
 
 
 for row in dataReader:
-    item=Style()
-    item.name = row[0]
-    item.save()
+#    item=Style()
+ #   item.name = row[0]
+  #  item.save()
     des = Designer()
-    des.name = row[1]
+    des.name = row[0]
     des.save()
 
         
